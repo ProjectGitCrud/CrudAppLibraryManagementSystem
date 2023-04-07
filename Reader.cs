92a19407059dc3f11d54abcca69c7cd222a17e79
ProjectGitCrud <130153675+ProjectGitCrud@users.noreply.github.com>
2023-04-07 18:02:34 +0530

    Add files via upload

---
+++ Reader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAppLibraryManagementSystem.Models
+{
+    public class Reader
+    {
+        //Attributes of table Reader in CrudAppLibraryManagementSystem database. 
+        public int ReaderId { get; set; }
+
+        public string ReaderName { get; set; }
+
+        public string ReaderAddress { get; set; }
+
+        public string ReaderContactNumber { get; set; }
+    }
+}
