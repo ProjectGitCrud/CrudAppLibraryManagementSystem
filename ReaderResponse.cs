a449811c61b19ac73ca19b3dc4a0fb61279306b1
ProjectGitCrud <130153675+ProjectGitCrud@users.noreply.github.com>
2023-04-07 18:03:10 +0530

    Add files via upload

---
+++ ReaderResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAppLibraryManagementSystem.Models
+{
+    public class ReaderResponse
+    {
+        public int StatusCode { get; set; }
+
+        public string StatusMessage { get; set; }
+
+        public Reader Reader { get; set; }
+
+        public List<Reader> ListReaders { get; set; }
+    }
+}
